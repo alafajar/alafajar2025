17e3ffd06aff9697e50cedd729762c7513c5f173
Fajar Muhamad <alafajar@gmail.com>
2025-04-19 13:38:45 +0700

    add: header component in page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,6 +11,31 @@ const Header = () => {
           Hi! I'm William Mark{" "}
           <Image src={assets.hand_icon} alt="" className="w-6" />
         </h3>
+        <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-ovo">
+          Frontend Web Developer based in London
+        </h1>
+        <p className="max-w-2xl mx-auto font-ovo">
+          I'am a frontend developer from California, USA with 10 years of
+          experience in multiple companies like Microsoft, Tesla and Apple
+        </p>
+        <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+          <a
+            href="#contact"
+            className="px-10 py-3 border boder-white rounded-full bg-black text-white flex items-center gap-2"
+          >
+            Contact Me{" "}
+            <Image src={assets.right_arrow_white} alt="" className="w-4" />
+          </a>
+
+          <a
+            href="/sample-resume.pdf"
+            download
+            className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
+          >
+            My Resume
+            <Image src={assets.download_icon} alt="" className="w-4" />
+          </a>
+        </div>
       </div>
     </div>
   );
