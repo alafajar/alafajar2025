51766d834946a1b9a2a194fa1dcca9917523e664
Fajar Muhamad <alafajar@gmail.com>
2025-04-19 16:00:49 +0700

    update: finishing work component

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -1,3 +1,5 @@
+import { assets, workData } from "@/assets/assets";
+import Image from "next/image";
 import React from "react";
 
 const Work = () => {
@@ -9,6 +11,38 @@ const Work = () => {
         Welcome to my web development portforlio! Explore a collection of
         projects showcasing my expertise in front-end development.
       </p>
+
+      <div className="grid grid-cols-(--grid-cols-auto) my-10 gap-5">
+        {workData.map(({ title, description, bgImage }, index) => (
+          <div
+            key={index}
+            style={{ backgroundImage: `url(${bgImage})` }}
+            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div>
+                <h2 className="font-semibold">{title}</h2>
+                <p className="text-sm text-gray-700">{description}</p>
+              </div>
+              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                <Image src={assets.send_icon} alt="send icon" className="w-5" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <a
+        href=""
+        className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-light-hover duration-500"
+      >
+        Show More{" "}
+        <Image
+          src={assets.right_arrow_bold}
+          alt="Right Arrow"
+          className="w-4"
+        />
+      </a>
     </div>
   );
 };
