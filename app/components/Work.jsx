0c088855ba46cc292678b02f1a77efc1f36da7f2
Fajar Muhamad <alafajar@gmail.com>
2025-04-19 15:43:37 +0700

    add: initial work component

---
+++ app/components/Work.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div id="work" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-ovo">My Portfolio</h4>
+      <h2 className="text-center text-5xl font-ovo">My Latest Work</h2>
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo">
+        Welcome to my web development portforlio! Explore a collection of
+        projects showcasing my expertise in front-end development.
+      </p>
+    </div>
+  );
+};
+
+export default Work;
